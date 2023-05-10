659fe3d27f1039597d43e2ca3be6dabca3632d5e
BrunaGbispo <bruna.araujobispo@gmail.com>
2023-05-09 23:35:04 -0300

    centralização card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import CardCarro from './Componentes/cardCarro/cardCarro';
+import Nissan from  "./Imagens/nissan.webp"
 
 function App() {
   return (
     <div className="App">
+      <CardCarro imgCarroCard={Nissan} modeloCarroCard="Nissan V8" preçoCarroCard="R$150.000,00"/>
+      <CardCarro imgCarroCard={Nissan} modeloCarroCard="Compass" preçoCarroCard="R$180.000,00"/>
+      <CardCarro imgCarroCard={Nissan} modeloCarroCard="Credito v8" preçoCarroCard="R$250.000,00"/>
     </div>
   );
 }
