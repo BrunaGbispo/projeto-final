1dbcf03aa38b255d9cb43c32193fd05ece17aba7
BrunaGbispo <bruna.araujobispo@gmail.com>
2023-05-12 20:21:06 -0300

    remove page

--- src/Componentes/cardCarro/cardCarro.jsx
+++ src/Componentes/cardCarro/cardCarro.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./cardCarro.css";
-import axios from "axios";
 
 export default function CardCarro (props){
 
