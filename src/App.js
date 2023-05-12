37cf858f0aab8533d2d5b0cf449d97e0dbe413b7
Aline Duarte <duartealine256@gmail.com>
2023-05-12 02:21:25 -0300

    estruturação componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Titulo from './Componentes/Titulo/Titulo';
+import Veiculos from './Componentes/Veiculos/Veiculos';
+import Footer from './Componentes/Footer/Footer';
+import Header from './Componentes/Header/Header';
 
 function App() {
   return (
     <div className="App">
+       <Header/>
+       <Titulo texto="Veículos"/>
+       <Veiculos />
+       <Footer/>
+
     </div>
   );
 }
