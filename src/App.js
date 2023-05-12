2afe8ac2f542bae47aaceb93ef84a470af249288
Marisa-Basilio <edna189@hotmail.com>
2023-05-12 19:44:36 -0300

    Criando a página compra
    Criando uma página contendo um card de compra.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import Compra from './Pages/Compra/Compra';
+import { BrowserRouter as Router, Route, Routes
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Router>
+          <Routes> 
+            <Route path="/" element={<Compra />} exact />
+          </Routes>
+        </Router>
     </div>
+
+  
   );
 }
 
