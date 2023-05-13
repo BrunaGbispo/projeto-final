c63a7457b88f7ddd4d85632eb6932775bd7b72b5
BrunaGbispo <bruna.araujobispo@gmail.com>
2023-05-12 21:26:24 -0300

    remove

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
-import Exposição from './Componentes/exposicao/exposição';
-import Veiculos from './pages/veiculos/veiculos';
-import CardCarro from './Componentes/cardCarro/cardCarro';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Routes>
-
-      </Routes>
-      </Router>
     </div>
   );
 }
