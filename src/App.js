767c463ee6e015c4674d3a424d14db32e1571b9c
BrunaGbispo <bruna.araujobispo@gmail.com>
2023-05-13 10:25:22 -0300

    render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import Veiculos from './Componentes/Veiculos/Veiculos';
 import Compra from './Pages/Compra/Compra';
 import { BrowserRouter as Router, Route, Routes
 } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <Router>
           <Routes> 
-            <Route path="/" element={<Compra />} exact />
+          <Route path="/" element={<Veiculos />} exact />         
+            <Route path="/compra" element={<Compra />} exact />
           </Routes>
         </Router>
     </div>
--- src/Componentes/Veiculos/Veiculos.jsx
+++ src/Componentes/Veiculos/Veiculos.jsx
@@ -1,12 +1,16 @@
 import React from 'react'; 
 import './Veiculos.css'
 import CardCarro from '../cardCarro/cardCarro';
+import Titulo from '../Titulo/Titulo';
 
 function Veiculos(props) {
   const { veiculos } = props;
 
   return (
     <div>
+    <Titulo texto="Veículos"/>
+    <CardCarro imgCarroCard="" modeloCarroCard="" preçoCarroCard=""/>
+    <CardCarro imgCarroCard="" modeloCarroCard="" preçoCarroCard=""/>
     <CardCarro imgCarroCard="" modeloCarroCard="" preçoCarroCard=""/>
     </div>
   );
