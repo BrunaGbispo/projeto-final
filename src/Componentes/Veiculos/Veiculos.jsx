113f95457c0f2e98bf98e2063b5748a2c641ba88
BrunaGbispo <bruna.araujobispo@gmail.com>
2023-05-13 11:33:41 -0300

    save cards veiculos

--- src/Componentes/Veiculos/Veiculos.jsx
+++ src/Componentes/Veiculos/Veiculos.jsx
@@ -1,17 +1,36 @@
-import React from 'react'; 
+import React, {useEffect, useState} from 'react';
 import './Veiculos.css'
 import CardCarro from '../cardCarro/cardCarro';
 import Titulo from '../Titulo/Titulo';
+import { veiculosImg } from '../../Imagens/obj';
+import axios from 'axios';
 
 function Veiculos(props) {
-  const { veiculos } = props;
+  const [details, setDetails] = useState(null)
+
+  useEffect(() => {
+    axios.get(`https://api-equipe-5.onrender.com/itens/`)
+      .then(response => {
+        setDetails(response.data.data);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, []);
+
+  if (!details) {
+    return (<h2>carregando...</h2>)
+  }
+
 
   return (
-    <div>
-    <Titulo texto="Veículos"/>
-    <CardCarro imgCarroCard="" modeloCarroCard="" preçoCarroCard=""/>
-    <CardCarro imgCarroCard="" modeloCarroCard="" preçoCarroCard=""/>
-    <CardCarro imgCarroCard="" modeloCarroCard="" preçoCarroCard=""/>
+    <div className='container'>
+      <Titulo texto="Veículos" />
+      <div className='top'>
+      <CardCarro imgCarroCard={veiculosImg[8]} modeloCarroCard={details[0].modelo} preçoCarroCard={details[0].preco} />
+      <CardCarro imgCarroCard={veiculosImg[9]} modeloCarroCard={details[1].modelo} preçoCarroCard={details[1].preco} />
+      <CardCarro imgCarroCard={veiculosImg[10]} modeloCarroCard={details[1].modelo} preçoCarroCard={details[1].preco} />
+      </div>
     </div>
   );
 }
