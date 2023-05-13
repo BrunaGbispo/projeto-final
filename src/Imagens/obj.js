5c628ce91194b0383fc3d572ce102a59c141f1cd
BrunaGbispo <bruna.araujobispo@gmail.com>
2023-05-13 01:40:58 -0300

    consumimdo api

---
+++ src/Imagens/obj.js
@@ -0,0 +1,10 @@
+export let veiculosImg =[ 
+    "https://quatrorodas.abril.com.br/wp-content/uploads/2022/02/Volvo-XC90-2020-1280-03-e1645048901939.jpg?quality=70&strip=info&w=1107&h=720&crop=1",
+    "https://s3.ecompletocarros.dev/images/lojas/285/veiculos/122577/veiculoInfoVeiculoImagesMobile/vehicle_image_1669936876_d41d8cd98f00b204e9800998ecf8427e.jpeg",
+    "https://s2.glbimg.com/ocWguh3bOoWVIShGAM6OrGyyJtM=/0x0:940x628/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/R/o/YANM7DSYyOlK5QqM6cyQ/2020-03-10-volvo-xc40-frente.jpg",
+    "https://bocamafrapremium.com.br/wp-content/uploads/2023/02/ac974163bec24085814c687618f29591_1675184621743.jpg",
+    "https://s2.glbimg.com/PKPZoFmGNir4dZuiHjtyxpwcy4A=/0x0:1980x1320/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/K/B/2XTT4uQ0elx739kirHKQ/jeep-renegade-33.jpeg",
+    "https://live.dealer-asset.co/images/br1001/product/paintSwatch/vehicle/ford-brasil-bronco-sport-verde-fuji-27032023.jpg?s=1024",
+    "https://quatrorodas.abril.com.br/wp-content/uploads/2021/05/range_rover_evoque_49.jpg?quality=70&strip=info",
+    "https://quatrorodas.abril.com.br/wp-content/uploads/2018/06/audi-a4-2019-1600-03-e1530114601340.jpg?quality=70&strip=info"
+]
