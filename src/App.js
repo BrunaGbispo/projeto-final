e5f2e31faedf575eea3ff2d5cfadc03701d71bae
Izabela Bertozo Reis <104469889+Izabelabertozoreis@users.noreply.github.com>
2023-05-13 01:01:03 -0300

    CarroDetalhes
    CarroDetalhes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Compra from './Pages/Compra/Compra';
+import CarroDetalhes from './Componentes/CarrosDetalhes/CarrosDetalhes';
+import Imagemcarro from './Imagens/ford-mustang_2015_01.jpg'
+import Imagemcarro1 from  './Imagens/Jeep Renegade 2016-min.jpg'
+
 import { BrowserRouter as Router, Route, Routes
 } from "react-router-dom";
 function App() {
@@ -10,6 +14,17 @@ function App() {
             <Route path="/" element={<Compra />} exact />
           </Routes>
         </Router>
+        <CarroDetalhes
+         image={Imagemcarro}
+         name="Ford Mustang"
+         details={CarroDetalhes}
+       />
+       <CarroDetalhes
+         image={Imagemcarro1}
+         name="Ford Mustang"
+         details={CarroDetalhes}
+       />
+       
     </div>
 
   
--- src/Componentes/CarrosDetalhes/CarrosDetalhes.jsx
+++ src/Componentes/CarrosDetalhes/CarrosDetalhes.jsx
@@ -1,1 +1,33 @@
 import React from "react";
+
+import PropTypes from 'prop-types';
+
+  
+
+  const CarroDetalhes = ({ image, name, details }) => {
+    return (
+      <div className="card mb-3">
+        <img src={image} className="card-img-top" alt={name} />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <ul className="list-group list-group-flush">
+            {Object.keys(details).map(key => (
+              <li key={key} className="list-group-item">
+                <strong>{key}:</strong> {details[key]}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  CarroDetalhes.props = {
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    details: PropTypes.string.isRequired,
+  };
+  
+
+  export default CarroDetalhes;
+
+  
