29ddf3c671bc363a846ff7c35f39990d33c47a89
BrunaGbispo <bruna.araujobispo@gmail.com>
2023-05-13 02:19:13 -0300

    renderizando card

--- src/Componentes/Veiculos/Veiculos.jsx
+++ src/Componentes/Veiculos/Veiculos.jsx
@@ -1,12 +1,13 @@
 import React from 'react'; 
 import './Veiculos.css'
+import CardCarro from '../cardCarro/cardCarro';
 
 function Veiculos(props) {
   const { veiculos } = props;
 
   return (
     <div>
-      
+    <CardCarro imgCarroCard="" modeloCarroCard="" preçoCarroCard=""/>
     </div>
   );
 }
